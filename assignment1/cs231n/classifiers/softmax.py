74961c5f5f8f271749204ce6c08e48c33eb8496f
Alexander Xydes <alexander.xydes@navy.mil>
2016-01-04 13:48:20 -0800

    Assgn1: Finished softmax code.

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -24,7 +24,36 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  # compute the loss and the gradient
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = W.dot(X[:, i])
+    scores -= np.max(scores)
+    num = np.exp(scores)
+    denom = num.sum()
+    pred = num[y[i]]/denom
+    loss += -np.log(pred)
+
+    d_pred = -1./pred
+
+    for j in xrange(num_classes):
+      if j == y[i]:
+        d_j = d_pred * (denom - num[y[i]])/denom**2
+      else:
+        d_j = d_pred * -num[y[i]]/denom**2
+      d_j = d_j * np.exp(scores[j])
+      dW[j,:] += d_j * X[:, i]
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -48,7 +77,23 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
+  train_idx = np.arange(num_train)
+
+  scores = W.dot(X)
+  scores -= np.max(scores, axis=0)
+  num = np.exp(scores)
+  denom = num.sum(axis=0)
+  loss = np.mean(-scores[y,train_idx] + np.log(denom))
+  loss += 0.5 * reg * np.sum(W * W)
+
+  dscores = num/denom
+  dscores[y, train_idx] = -1
+  dW = dscores.dot(X.T)
+  dW /= num_train
+  dW += reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
